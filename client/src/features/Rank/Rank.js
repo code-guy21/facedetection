b51b7ad5fc48c8f22612d38ce3f4e36f31463e9c
code-guy21 <alexissj16@knights.ucf.edu>
2018-07-17 18:27:51 -0400

    updates

--- client/src/features/Rank/Rank.js
+++ client/src/features/Rank/Rank.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Rank = ({ userName, rank }) => {
 	return (
 		<div>
-			<div className="white f3">{`${userName}, your current entry count is...`}</div>
+			<div className="white f3">{`${userName}, your current entry count is..`}</div>
 			<div className="white f1">{rank}</div>
 		</div>
 	);
