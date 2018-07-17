84f7284bd34c71f58533b256995e3c2d99f96f83
code-guy21 <alexissj16@knights.ucf.edu>
2018-07-17 16:23:17 -0400

    database

--- index.js
+++ index.js
@@ -6,10 +6,8 @@ const bcrypt = require('bcrypt-nodejs');
 const db = knex({
 	client: 'pg',
 	connection: {
-		host: '127.0.0.1',
-		user: 'alexissanjavier',
-		password: '',
-		database: 'smart-brain'
+		host: process.env.DATABASE_URL,
+		ssl: true
 	}
 });
 
